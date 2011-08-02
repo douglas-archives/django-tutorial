e570049430dc7d4e86704436157db699a2344507
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-08-01 23:43:11 -0400

    now i get the last polls ordened by pub_date and show in view

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,10 @@
+from polls.models import Poll
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello, World. You're at the poll index.")
+	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+	output = ', '.join([p.question for p in latest_poll_list])
+	return HttpResponse(output)
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
@@ -10,4 +13,4 @@ def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)	
+    return HttpResponse("You're voting on poll %s." % poll_id)
