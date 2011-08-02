ce362eecdf1f1cc337950902cdbf433d4374bc3c
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-08-01 22:59:45 -0400

    creating the url patterns

--- urls.py
+++ urls.py
@@ -5,13 +5,15 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	(r'^polls/$', 'polls.views.index'),
+	(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+	(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+	(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+    url(r'^admin/', include(admin.site.urls)),
     # Examples:
     # url(r'^$', 'django_tutorial.views.home', name='home'),
     # url(r'^django_tutorial/', include('django_tutorial.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 )
