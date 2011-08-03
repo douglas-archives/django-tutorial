5dfe5c53685166747294e69379142418dde99472
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-08-02 23:18:57 -0400

    optimizing with renter_to_response

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,6 @@
-from django.template import Context, loader
+from django.shortcuts import render_to_response
 from polls.models import Poll
-from django.http import HttpResponse
 
 def index(request):
 	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-	t = loader.get_template('polls/index.html')
-	c = Context({
-		'latest_poll_list' : latest_poll_list
-	})
-	return HttpResponse(t.render(c))
-
-def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
-
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
-
-def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+	return render_to_response('polls/index.html', {'latest_poll_list' : latest_poll_list})
