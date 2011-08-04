77c5a874e5ff0cfd99f84723663c2b597958b1ca
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-08-03 22:10:35 -0400

    on better way to render the views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from polls.models import Poll
 
 def index(request):
 	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
 	return render_to_response('polls/index.html', {'latest_poll_list' : latest_poll_list})
+
+def detail(request, poll_id):
+	p = get_object_or_404(Poll, pk=poll_id)
+	return render_to_response('polls/detail.html', {'poll': p})
